5fffa52d05a47935b5406653a75111ac4c8615d0
giagra91 <giagra91@gmail.com>
2022-05-26 15:59:46 +0200

    showed single cards with Vue in guest  page

---
+++ resources/js/components/Post.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="col-4">
+        <div class="card p-3 my-2">
+            <img :src="post.image_url" class="card-img-top img-fluid" :alt="`Image of ` + post.title">
+            <div class="card-body">
+                <h5 class="card-title">{{ post.title }}</h5>
+                <p class="card-text">{{ post.content.substring(0, 100) + "..." }}</p>
+            </div>
+            <div class="card-body">
+                <p>Created: {{ post.created_at.substring(0, 10) }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Post",
+    props:["post"]
+}
+</script>
+
+<style>
+
+</style>
--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,3 +1,31 @@
-<h1>
-    Benvenuto nella pagina riservata dei guest
-</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset("css/app.css") }}">
+    <title>Guest Home</title>
+</head>
+<body>
+    <div class="flex-center position-ref full-height">
+        @if (Route::has('login'))
+            <div class="top-right links">
+                @auth
+                    <a href="{{ route('admin.home') }}">Home</a>
+                    <a href="{{ route('admin.posts.index') }}">Posts</a>
+                @else
+                    <a href="{{ route('login') }}">Login</a>
+
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}">Register</a>
+                    @endif
+                @endauth
+            </div>
+        @endif
+    </div>
+    <div id="root"></div>
+
+    <script src="{{ asset("js/front.js") }}"></script>
+</body>
+</html>
